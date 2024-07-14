84020ac9ee691b5d998a0d79538c22c4e523d6c1
nakajima97 <kituneudon97@gmail.com>
2024-07-14 17:42:07 +0900

    #4 biomeを実行

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,5 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	return (
-			<main>
-				Hello World!!
-			</main>
-	);
+	return <main>Hello World!!</main>;
 }
